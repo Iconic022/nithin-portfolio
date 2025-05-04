25df6d6d69170762bc3cecfc97e5ed344108ec6b
Gadaveni Nithin <117366307+Iconic022@users.noreply.github.com>
2025-05-04 14:04:28 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nithin-portfolio/',
+  base: '/nithin-portfolio/',  // <-- Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
 
